fcf818f642b757b56ffb2e1a6c4f531c0d9fa096
Paulo Salvatore <salvatoregames@gmail.com>
2022-11-30 21:18:06 -0300

    Create implementado

--- backend/src/item/item.controller.js
+++ backend/src/item/item.controller.js
@@ -21,7 +21,17 @@ const findById = (req, res) => {
   res.send(item);
 };
 
-const create = (req, res) => {};
+const create = (req, res) => {
+  const item = req.body;
+
+  if (!item || !item.name || !item.imageUrl || !item.category) {
+    return res.status(400).send({ message: "Dados inválidos." });
+  }
+
+  const newItem = {};
+
+  res.status(201).send(newItem);
+};
 
 const update = (req, res) => {};
 
